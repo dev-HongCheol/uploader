e001843de73a3c09cbd7e59c6ae0902ff785767e
dev-HongCheol <choisieun89@gmail.com>
2023-10-27 10:11:59 +0900

    파일 개수 제한, 사이즈 제한 기능 추가

--- src/uploader.js
+++ src/uploader.js
@@ -1,13 +1,25 @@
 const fileUploader = {
   uploaderInput: null,
   uploaderListDiv: null,
-  uploadFiles: null,
+  uploadFileMaxLenght: 3,
+  // ** 뒤 숫자 > (1:KB, 2:MB , 3:GB ... )
+  uploadFileMaxSize: 1024 ** 2 * 10,
+  uploadFileDataTranster: null,
   /**
-   * 화면이 모두 로드된 후 input의 .ma-uploader를 찾아 onchange 이벤트 바인딩 및
-   * 업로드 파일정보 리스트 출력함수 showFileInfoList 호출
+   * 화면이 모두 로드된 후 input의 .ma-uploader를 찾아 onchange 이벤트 바인딩
+   * 1. 최대 업로드 가능 사이즈(uploadFileMaxSize) 체크 -> 초과할 경우 리스트 미출력
+   * 2. 최대 업로드 파일 갯수(uploadFileMaxLenght) 체크
+   * 3. 리스트 출력
    */
   bind: () => {
     window.addEventListener('DOMContentLoaded', () => {
+      console.info(
+        `fileUploader __ uploadFileMaxLenght : ${
+          fileUploader.uploadFileMaxLenght
+        } / uploadFileMaxSize : ${fileUploader.formatSize(
+          fileUploader.uploadFileMaxSize,
+        )}`,
+      );
       fileUploader.uploaderInput = document.querySelector('.ma-uploader');
       if (!fileUploader.uploaderInput) {
         console.error('업로더 input .ma-uploader가 존재하지 않습니다.');
@@ -24,21 +36,107 @@ const fileUploader = {
           );
           return;
         }
+
+        fileUploader.uploadFileDataTranster = new DataTransfer();
+
+        const isMaxFileSizeOver = fileUploader.uploadFileMaxSizeCheck();
+        if (isMaxFileSizeOver) {
+          alert(
+            `업로드 최대 사이즈 ${fileUploader.formatSize(
+              fileUploader.uploadFileMaxSize,
+            )}를 초과하였습니다.`,
+          );
+          fileUploader.uploaderInput.value = null;
+          fileUploader.showFileInfoList();
+          return;
+        }
+        fileUploader.uploadFileMaxLengthCheck();
         fileUploader.showFileInfoList();
       });
     });
+  },
+
+  /* uploadFileMaxSizeCheck: () => {
+    const uploadFiles = fileUploader.uploaderInput.files;
+    const fileUploaderArr = Array.from(uploadFiles);
+    fileUploader.uploadFileDataTranster.items.clear();
+    let totalUploadSize = 0;
+
+    fileUploaderArr.some((file) => {
+      totalUploadSize += file.size;
+      if (totalUploadSize > fileUploader.uploadFileMaxSize) {
+        alert(
+          `업로드 최대 사이즈(${fileUploader.formatSize(
+            fileUploader.uploadFileMaxSize,
+          )})를 초과하여 일부파일만 업로드 됩니다.`,
+        );
+        return true;
+      }
+      fileUploader.uploadFileDataTranster.items.add(file);
+    });
+
+    fileUploader.uploaderInput.files = fileUploader.uploadFileDataTranster.files;
+  }, */
+
+  /**
+   * 업로드 파일 사이즈 체크 함수
+   * @returns 파일의 전체 사이즈가 제한 사이즈(uploadFileMaxSize) 오버 여부
+   */
+  uploadFileMaxSizeCheck: () => {
+    const uploadFiles = fileUploader.uploaderInput.files;
+    const fileUploaderArr = Array.from(uploadFiles);
+    fileUploader.uploadFileDataTranster.items.clear();
+    let totalUploadSize = 0;
+
+    const isMaxFileSizeOver = fileUploaderArr.some((file) => {
+      totalUploadSize += file.size;
+      if (totalUploadSize > fileUploader.uploadFileMaxSize) {
+        return true;
+      }
+      fileUploader.uploadFileDataTranster.items.add(file);
+    });
+
+    fileUploader.uploaderInput.files = fileUploader.uploadFileDataTranster.files;
+    return isMaxFileSizeOver;
+  },
+
+  /**
+   * 선택한 파일의 수가 최대 파일 수(uploadFileMaxLenght)를 넘을 경우 최대 파일 수 만큼만 선택되어 업로드 됨
+   */
+  uploadFileMaxLengthCheck: () => {
+    const uploadFiles = fileUploader.uploaderInput.files;
+    const fileUploaderArr = Array.from(uploadFiles);
+    fileUploader.uploadFileDataTranster.items.clear();
+
+    if (fileUploaderArr.length > fileUploader.uploadFileMaxLenght) {
+      fileUploaderArr.some((file, index) => {
+        if (index === fileUploader.uploadFileMaxLenght) {
+          alert(
+            `업로드 최대 개수 ${fileUploader.uploadFileMaxLenght}개를 초과하여 ${fileUploaderArr.length}개 중 ${fileUploader.uploadFileMaxLenght}개만 업로드 됩니다.`,
+          );
+          return true;
+        }
+        fileUploader.uploadFileDataTranster.items.add(file);
+      });
+    } else {
+      fileUploaderArr.forEach((file) => {
+        fileUploader.uploadFileDataTranster.items.add(file);
+      });
+    }
+
+    fileUploader.uploaderInput.files = fileUploader.uploadFileDataTranster.files;
   },
 
   /**
    * 업로드 파일정보 리스트 초기화 및 정보 출력
    */
   showFileInfoList: () => {
-    fileUploader.uploadFiles = fileUploader.uploaderInput.files;
     const maFileInfos = fileUploader.uploaderListDiv.querySelectorAll('.ma-file-info');
     maFileInfos.forEach((maFileInfo) => maFileInfo.remove());
 
-    for (let fIndex = 0; fIndex < fileUploader.uploadFiles.length; fIndex += 1) {
-      const file = fileUploader.uploadFiles[fIndex];
+    const uploadFiles = fileUploader.uploaderInput.files;
+    for (let fIndex = 0; fIndex < uploadFiles.length; fIndex += 1) {
+      const file = uploadFiles[fIndex];
       const fileListHtml = `
           <div class="ma-file-info" id="maUploadList-${fIndex}">
               <div class="file-no">${fIndex + 1}</div>
@@ -64,14 +162,15 @@ const fileUploader = {
     if (!isDelete) {
       return;
     }
-    const dataTranster = new DataTransfer();
 
-    Array.from(fileUploader.uploadFiles)
-      .filter((file, index) => index !== fIndex)
+    const fileUploaderArr = Array.from(fileUploader.uploadFileDataTranster.files);
+    fileUploader.uploadFileDataTranster.items.clear();
+    fileUploaderArr
+      .filter((_file, index) => index !== fIndex)
       .forEach((file) => {
-        dataTranster.items.add(file);
+        fileUploader.uploadFileDataTranster.items.add(file);
       });
-    fileUploader.uploaderInput.files = dataTranster.files;
+    fileUploader.uploaderInput.files = fileUploader.uploadFileDataTranster.files;
     fileUploader.showFileInfoList();
   },
 
@@ -80,7 +179,7 @@ const fileUploader = {
    */
   formatSize: (length) => {
     let i = 0;
-    const type = ['b', 'Кb', 'Мb', 'Gb', 'Тb', 'Pb'];
+    const type = ['B', 'КB', 'МB', 'GB', 'ТB', 'PB'];
     while ((length / 1000) | 0 && i < type.length - 1) {
       length /= 1024;
       i++;
